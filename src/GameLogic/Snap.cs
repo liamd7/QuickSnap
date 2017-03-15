46a51d016dbb827b4d2d690f5a7df6790fb237c9
Liam Dullard <liamD_7@hotmail.com>
2017-03-15 16:57:21 +1100

    Play sound effects

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -138,7 +138,7 @@ namespace CardGames.GameLogic
 				 _topCards [0] != null && _topCards [0].Rank == _topCards [1].Rank) // and its a match
 			{
 				_score[player]++;
-				//TODO: consider playing a sound here...
+				SwinGame.LoadSoundEffectNamed ("Slap", "slap.mp3");
 			}
 
 			// stop the game...
